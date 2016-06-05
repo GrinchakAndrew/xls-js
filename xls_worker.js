0e4b0cfda7f76ee81a4a9a529acee5a613c8b5d5
GrinchakAndrew <andrewgrinchak@gmail.com>
2016-06-05 19:31:05 +0300

    changes and updates 6/05/16 : 19:31

--- xls_worker.js
+++ xls_worker.js
@@ -1,12 +1,17 @@
-
 var config = {
+	/*theWhat -> the number of sheets in the wb*/
 	theWhat : {},
 	sheetNames : '',
 	range : '',
 	newVal : '',
+	workSheet : '',
 	wb : '',
 	f : '',
-	fnArr : [function(el){
+	clientNames : '', 
+	tasksNames : '',
+	IDs : '',
+	IDs_plus_Tasks : [],
+	fnArr : [function(el) {
 			$(el).css('background-color') !== "rgba(0, 0, 0, 0)" ? 
 			$(el).css('background-color', '') :
 			$(el).css('background-color', '#CCEEFF');
@@ -21,7 +26,8 @@ var config = {
 		 });
 		 config.fnArr = [];
     }, 
-	init : function(){
+	
+	init : function() {
 		config.helper = [];
 		$('#drag-and-drop').on(
 		'dragover',
@@ -31,7 +37,9 @@ var config = {
 			'dragenter',
 		config.defPreventer);
 	},
-	
+	table_template : '<table>'+'<thead>'+'<tr></tr>'+'</thead>'+'<tbody></tbody>'+'</table>',
+	preview_template : '<div class="'+'table-preview">'+'</div>',
+	/*helper-function to construct html-ized xlsx table*/
 	onloadHandlerSub : function(i, a, wb, the_number_of_rows, b) {
 										if(wb.Sheets.hasOwnProperty(i)) {
 										for (var n = 0; n < the_number_of_rows; n++){
@@ -41,50 +49,77 @@ var config = {
 												var dataSet = wb.Sheets[i][la];
 												var textValue = dataSet['w'] ? dataSet['w'] : dataSet['v'];
 												if(b == 0) {
-													$('#table-preview tbody').append($("<tr></tr>", {"row": row, "column" : a}));
+													var selector1 = '.table-preview[sheet=' + '"' + i + '"'+']' + ' tbody';
+													var selector2 = '.table-preview[sheet=' + '"' + i + '"'+']' + ' tbody tr';
+													$(selector1).append($("<tr></tr>", {"row": row, "column" : a}));
+													$(selector2).last().append($("<td></td>", {"lineNum" : n}).text(row));
+													$(selector2).last().append($("<td></td>", {"ref" : a+row}).text(textValue));
+													/* $('#table-preview tbody').append($("<tr></tr>", {"row": row, "column" : a}));
 													$('#table-preview tbody tr').last().append($("<td></td>", {"lineNum" : n}).text(row));
-													$('#table-preview tbody tr').last().append($("<td></td>", {"ref" : a+row}).text(textValue));
+													$('#table-preview tbody tr').last().append($("<td></td>", {"ref" : a+row}).text(textValue)); */
 												}else{
-													var lookup = '#table-preview tbody tr[row="'+row+'"]';
+													//var lookup = '.table-preview tbody tr[row="'+row+'"]';
+													var lookup = '.table-preview[sheet=' + '"' + i + '"'+']' + ' tbody' + ' tr[row="'+row+'"]';
 													$(lookup).append($("<td></td>", {"ref" : a+row}).text(textValue));
 												}
 											} else if(parseInt(la.match(/\d+/)) !== 0) {
-												var lookup = '#table-preview tbody tr[row="'+ parseInt(la.match(/\d+/)) +'"]';
-												if(!$(lookup).length){
-													$('#table-preview tbody').append($("<tr></tr>", {"row": parseInt(la.match(/\d+/)), "column" : a}));
-													$('#table-preview tbody tr').last().append($("<td></td>", {"lineNum" : parseInt(la.match(/\d+/))}).text(parseInt(la.match(/\d+/))));
+												//var lookup = '.table-preview tbody tr[row="'+ parseInt(la.match(/\d+/)) +'"]';
+												var lookup1 = '.table-preview[sheet=' + '"' + i + '"'+']' + ' tbody' +' tr[row="'+ parseInt(la.match(/\d+/)) +'"]';
+												var lookup2 = '.table-preview[sheet=' + '"' + i + '"'+']' + ' tbody';
+												var lookup3 = '.table-preview[sheet=' + '"' + i + '"'+']' + ' tbody tr';
+												if(!$(lookup1).length){
+													$(lookup2).append($("<tr></tr>", {"row": parseInt(la.match(/\d+/)), "column" : a}));
+													$(lookup3).last().append($("<td></td>", {"lineNum" : parseInt(la.match(/\d+/))}).text(parseInt(la.match(/\d+/))));
 												}
-												$(lookup).append($("<td></td>", {"ref" : a+ row }).text("")); 
+												$(lookup1).append($("<td></td>", {"ref" : a+ row }).text("")); 
 											}
 										}
 									}
 			}, 
 	htmlize : function() {
-		config.wb.SheetNames.forEach(function(i, j) {
-						if(config.wb.Sheets.hasOwnProperty(i)){
+		var subRoutine = function(i) {
+			if(config.wb.Sheets.hasOwnProperty(i)) {
+				/*appending the table-preview into the wrapper && the tableTab per each sheet*/
+				var parser = new DOMParser(), 
+					tableTab = parser.parseFromString(config.table_template, "text/html"), 
+					tablePreview = parser.parseFromString(config.preview_template, "text/html");
+					tablePreview = tablePreview.querySelector('.table-preview');
+					tableTab = tableTab.querySelector('table');
+					tablePreview.setAttribute('sheet', i);
+					tableTab.setAttribute('sheet', i), 
+					selector = '.table-preview[sheet=' + '"' + i + '"' + ']'; 
+					document.querySelector('#wrapper').appendChild(tablePreview);
+					document.querySelector(selector).appendChild(tableTab);
 							var range = config.wb.Sheets[i]['!ref'];
 							var the_number_of_rows = parseInt(range.split(':')[1].match(/\d+/)[0]);
 							var letterRanges = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 							letterRanges.forEach(function(a, b) {
-								var row = a+'0';
+								var row = a+'0', 
+									selector = '.table-preview table[sheet="'+ i + '"' + ']' + ' thead tr';
 								if(b == 0) {
-									$('#table-preview thead tr').first().append($("<td></td>", {"lineNum": row}).text('#'));
-									$('#table-preview thead tr').first().append($("<td></td>", {"row": row}).text(a));
+									$(selector).first().append($("<td></td>", {"lineNum": row}).text('#'));
+									$(selector).first().append($("<td></td>", {"row": row}).text(a));
 									config.onloadHandlerSub(i, a, config.wb, the_number_of_rows, b);
 								}else if(Object.keys(config.wb['Sheets'][i]).some(function(el, i, arr) {
 											 var rx = new RegExp(a+"\\d?");
 											 return el.match(rx);
 										})) {
-									$('#table-preview thead tr').first().append($("<td></td>", {"row": row}).text(a));
+									$(selector).first().append($("<td></td>", {"row": row}).text(a));
 									config.onloadHandlerSub(i, a, config.wb, the_number_of_rows, b);
 								}
 							});							
-						}
-					});
+			}
+		};
 		
+		config.wb.SheetNames.forEach(function(i, j) {
+			/*before calling the html-ize subRoutine, need to make sure each 
+			 sheet is fitted to its own html tab!*/
+			subRoutine(i);
+		});
 	}, 
 	row : '',
 	lineNum : '',
+	
 	colorSubroutine : function(el) {
 		if(el && $(el).css('backgroundColor') && ($(el).css('background-color') == "rgb(170, 170, 170)")){
 			$(el).css('backgroundColor', 'white');
@@ -130,7 +165,7 @@ var config = {
 					});
 				});
 				if(config.helper.length) {
-					Object.keys(config.theWhat).forEach(function(i, j){
+					Object.keys(config.theWhat).forEach(function(i, j) {
 						config.theWhat[i] = [];
 						config.helper.forEach(function(k, l){
 							var t = {};
@@ -143,17 +178,16 @@ var config = {
 		}
 	}, 
 	
-	processWb : function(){
-					/*processing the workbook here:*/
+	processWb : function() {
+				/*processing the workbook here:*/
+				debugger;
 					if(({}).toString.call(config.theWhat) == '[object Object]') {
 							for (var sheet in config.theWhat){
-								 
-								if(config.theWhat[sheet] && ({}).toString.call(config.theWhat[sheet]) == '[object Array]') {
-									 
+								debugger;
+								if(config.theWhat[sheet] && ({}).toString.call(config.theWhat[sheet]) == '[object Array]' && config.workSheet == sheet) {
 									config.theWhat[sheet].forEach(function(i, j) {
-										 
+										debugger;
 										for (var cell in i) {
-											 
 											if(cell.match(/[-]/)) {
 											/*we have got a range!*/
 												var s = cell.split('-')[0];
@@ -208,7 +242,8 @@ $('#drag-and-drop').on(
 				reader.onload = function(e) {
 					var data = e.target.result;
 					config.wb = XLSX.read(data, {type: 'binary'});
-					config.sheetNames = config.wb.SheetNames;
+					/*get the number of worksheets, i.e., the tabs:*/
+					config.sheetNames = config.wb.SheetNames; 
 					config.sheetNames.forEach(function(i, j){
 						 config.theWhat[i] = [{}];
 					});
@@ -238,24 +273,28 @@ $('#drag-and-drop').on(
     }
 );
 	$(document).on('click', function(e){config.onclicker(e)});
-	$('#textarea textarea').on('mousedown', function() {
+	/* $('#textarea textarea').on('mousedown', function() {
 		this.value = '';	
-	});
-
+	}); */
+	
+	/*processing the process the workbook btn*/
 	$('#process_wb').on('click', function() {
-		var val = $('textarea').val();
-		if(val &&  $('tbody').html() && $('textarea').val().match(/^\n?\s?\D+?\d+?\s?(?=[-])\s?[-]\s?\D+\d+\s?[:]\s?.*/)) {
-			if(val.match(/new Date/) && $('textarea').val().match(/^\n?\s?\D+?\d+?\s?(?=[-])\s?[-]\s?\D+\d+\s?[:]\s?.*/)){
+		debugger;
+		var val = $('textarea[id="range_new_val_text_area"]').val();
+		/* config.workSheet = $('textarea[id="ws_text_area"]').val(); */
+		
+		/*match if we have got the Date to set to the table*/
+		if(val &&  $('tbody').html() && $('textarea[id="range_new_val_text_area"]').val().match(/^\n?\s?\D+?\d+?\s?(?=[-])\s?[-]\s?\D+\d+\s?[:]\s?.*/)) {
+			if(val.match(/new Date/) && $('textarea[id="range_new_val_text_area"]').val().match(/^\n?\s?\D+?\d+?\s?(?=[-])\s?[-]\s?\D+\d+\s?[:]\s?.*/)){
 				config.range = val.match(/^\D+\d+[-]\D+\d+/);
 				config.range[0] = config.range[0].replace(/\s+/g, '');
 				var d = val.match(/[:](.*)/)[1];
 				config.newVal = JSDateToExcelDate(new Function("return " + d+";")().getTime())
-			}else if ($('textarea').val().match(/^\n?\s?\D+?\d+?\s?(?=[-])\s?[-]\s?\D+\d+\s?[:]\s?.*/)){
+			}else if (val.match(/^\n?\s?\D+?\d+?\s?(?=[-])\s?[-]\s?\D+\d+\s?[:]\s?.*/)){
 				val = val.replace(/\s+/g, '');
 				config.range = val.match(/^\D+\d+[-]\D+\d+/);
-				config.newVal = val.match(/[:](.*)$/)[1];	
+				config.newVal = val.match(/[:](.*)$/)[1];
 			}
-			
 			Object.keys(config.theWhat).forEach(function(i, j) {
 				if(config.theWhat[i].forEach){
 					config.theWhat[i].forEach(function(z, w) {
@@ -264,26 +303,152 @@ $('#drag-and-drop').on(
 				}
 			});
 			config.processWb();
-		}else if(val &&  $('tbody').html() && ($('textarea').val().match(/^\n?\s?\d+\s?/) || $('textarea').val().match(/new Date/))) {
+		/*match if we have got the other data to set*/
+		}else if(val &&  $('tbody').html()) {
+			//automation specifically for Natalia 
 					// we end up here if the range was set by highlights: 
-					if($('textarea').val().match(/new Date/)){
+				var txtarea	= "";
+					txtarea = $('textarea[id="range_new_val_text_area"]').val().replace(" ", "");
+					txtarea = txtarea.match(/[{|A-Z|1-9|}].*/g);
+					if($('textarea[id="range_new_val_text_area"]').val().match(/new Date/)) {
 						var d = val;
 						config.newVal = JSDateToExcelDate(new Function("return " + d+";")().getTime())
 						val = config.newVal;
+						console.log("new Date part of code has been matched!");
 					}
-					Object.keys(config.theWhat).forEach(function(i, j) {
-						if(config.theWhat[i].forEach){
-								config.theWhat[i].forEach(function(z, w) {
-									z[Object.keys(z)[0]] = val;
-								});
+					/*we check if we have a customized change request to automate: enclose all custom requests with {}*/
+					if(txtarea[txtarea.length-1] == "}" && txtarea[0] == "{") {
+					//1. get All Client_Names -> array
+						var getItemNamesByColumn = function(workSheet, columnName, unique) {
+							var workbook = config.wb.Workbook.Sheets;
+							var letterRanges = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+							var returnable = [];
+							workbook.forEach(function(sheet) {
+								if(sheet['name'] == workSheet) {
+									var ref = config.wb.Sheets[sheet['name']]['!ref'];
+									var upperBound = parseInt(config.wb.Sheets[sheet['name']]['!ref'].split(':')[1].match(/\d+/));
+									letterRanges.forEach(function(letter){
+										var _columnName = config.wb.Sheets[sheet['name']][letter+1] ? config.wb.Sheets[sheet['name']][letter+1]['v'] : '';
+										var theLetter = '';
+											if(_columnName == columnName){
+												theLetter = letter;
+												while(upperBound > 1) {
+													config.wb.Sheets[sheet['name']][theLetter + upperBound] && 
+													config.wb.Sheets[sheet['name']][theLetter + upperBound]['v'] ? 
+													returnable.push(config.wb.Sheets[sheet['name']][theLetter + upperBound]['v']) : 
+													returnable; 
+													upperBound--;
+												}
+											}
+									});
+								}
+							});
+							return returnable;
+							
+						};
+						config.clientNames = getItemNamesByColumn('Campaigns', 'Client Name');
+						//2. get All the Tasks_Names -> array
+						config.tasksNames = getItemNamesByColumn('WBS', 'Task name')
+						//3. All IDs -> array
+						config.IDs = getItemNamesByColumn('Campaigns', 'ID');
+						//3.1 All IDs_+_Tasks -> array for SubTask Number 
+						//config.IDs_+_Tasks = 
+						//SubTask Number to place here! Final List
+						for (var i = config.IDs.length; i--; ){
+							 var returnable = [];
+								for(var j = config.tasksNames.length; j--; ){
+									console.log(config.IDs[i] + ' ' + config.tasksNames[j]);
+									returnable.push(config.IDs[i] + ' ' + config.tasksNames[j]);
+								}
+						    config.IDs_plus_Tasks.push(returnable);
+						}
+						/*
+							4. forEach -> Client_Names(Client_Name) {
+							4.1. Client_Name copyTo B2-B[Client_Names.length], Final List
+							4.2. IDs_+_Tasks.forEach(IDs_+_Task) {
+								 IDs_+_Task copyTo D2-D[IDs_+_Tasks.length], Final List
+							 }
+							4.3. Tasks_Names.forEach(Tasks_Name) {
+								Tasks_Name copyTo F2-F[Tasks_Names.length], Final List
 							}
-					});
+						}
+						*/
+						
+						var rangeSeeker = function(workSheet /*Final List*/, columnName /*Oracle Project Name*/) {
+							debugger;
+							var returnable = '';
+							var workbook = config.wb.Workbook.Sheets;
+							workbook.forEach(function(sheet) {
+								if(sheet['name'] == workSheet) {
+									debugger;
+									var ref = config.wb.Sheets[sheet['name']]['!ref'];
+									var upperBound = parseInt(config.wb.Sheets[sheet['name']]['!ref'].split(':')[1].match(/\d+/));
+									var higherBound = parseInt(config.wb.Sheets[sheet['name']]['!ref'].split(':')[0].match(/\d+/));
+									var _columnName = config.wb.Sheets[sheet['name']][higherBound]['v'];
+											if(_columnName == columnName) {
+												returnable = config.wb.Sheets[sheet['name']]['!ref']
+											}
+									});
+								}
+							return returnable;
+						};
+						
+						for(var i = config.clientNames.length; i--; ) {
+							debugger;
+							
+							var clientName = config.clientNames[i];
+							var range = ''
+							range = rangeSeeker('Final List', 'Oracle Project Name');
+							
+							Object.keys(config.theWhat).forEach(function(i, j) {
+								debugger;
+								if(config.theWhat[i].forEach){
+									debugger;
+										config.theWhat[i].forEach(function(z, w){
+											debugger;
+											z[Object.keys(z)[0]] = range;
+											
+											/*the writing mechanism: 
+												config.theWhat[sheet].forEach(function(i, j) {
+													debugger;
+													for (var cell in i) {
+														if(cell.match(/[-]/)) {
+														//we have got a range!
+															var s = cell.split('-')[0];
+															var e = cell.split('-')[1];
+															var sD = parseInt(s.match(/\d+/g)[0]);
+															var eD = parseInt(e.match(/\d+/g)[0]);
+															var rangeLetter = s.match(/\D+/g)[0];
+															for (var y = sD; y < eD+1; y++) {
+																if(config.wb.Sheets[sheet][rangeLetter+y]) {
+																	config.wb.Sheets[sheet][rangeLetter+y]['v'] = i[cell];
+																} else {
+																	config.wb.Sheets[sheet][rangeLetter+y] = {t: "n", v: i[cell], f: '', w: "0"};
+																}
+															}
+														} else {
+															if(config.wb.Sheets[sheet][cell]){
+															config.wb.Sheets[sheet][cell]['v'] = i[cell];
+															}else {
+																config.wb.Sheets[sheet][cell] = {t: "n", v: i[cell], f: '', w: "0"};
+															}
+														}
+													}
+												});
+											*/
+											
+										});
+									}
+							});
+							
+						}
+					}
 					config.processWb();
 		}		
 	});
+	/*onclick on the btn clear the workbook will clear the area with the html-ised workbook*/
 	$('#clear_wb').on('click', function(e){
-		$('table thead tr').html('');
-		$('table tbody').html('');
+		$('.table-preview').html('');
 	});
 	
 	
